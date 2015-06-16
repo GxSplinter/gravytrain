a2a5c4c52201a5269355f756a7576e01a4938930
GxSplinter <12126438@studentmail.ul.ie>
2015-06-16 17:28:20 +0100

    Adds feature for registering providers

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,2 +1,5 @@
 class Provider < ActiveRecord::Base
+  validates_acceptance_of :accepted_terms,
+  allow_nil: false,
+  accept: true
 end
---
+++ features/step_definitions/provider_steps.rb
@@ -0,0 +1,28 @@
+Given(/^no Providers exist$/) do
+  Provider.delete_all
+end
+
+When(/^I am on the registration page$/) do
+  visit new_provider_path
+end
+
+When(/^I complete the required fields$/) do
+  fill_in 'Name', with: 'Dave'
+  fill_in 'Address', with: '10 Pie Lane'
+  fill_in 'Postcode', with: 'RG2 9FL'
+  fill_in 'About me', with: 'Ben is a douche'
+  fill_in 'Paypal email', with: 'ding@dong.com'
+  check 'Accepted terms'
+end
+
+When(/^I submit my information$/) do
+  click_button 'Create Provider'
+end
+
+Then(/^my Provider account is created$/) do
+  expect(Provider.count).to eq (1)
+end
+
+Then(/^I see a confirmation message$/) do
+  expect(page).to have_content('Done!')
+end
